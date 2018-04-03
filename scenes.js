d32ea7364970003acb210324eb18b89177ff1052
Ivan Echevarria <iechevarria@email.wm.edu>
2018-04-03 00:28:18 -0400

    Update demo scene
    Put scene inside box, add lights, add spheres

--- scenes.js
+++ scenes.js
@@ -1,30 +1,80 @@
 let scene1 = new Scene();
 
 let white = new Material(new Color(0.8, 0.8, 0.8), new Color(0.2, 0.2, 0.2), true);
+let red = new Material(new Color(1, 0, 0), false, false);
 let mirror = new Material(new Color(0, 0, 0), new Color(1, 1, 1), true);
 
-scene1.addGeometry(new Sphere(new Vec3d(0, 0, -5), 0.5, white));
+scene1.addGeometry(new Sphere(new Vec3d(0.8, -1.3, -8), 1.2, mirror));
+scene1.addGeometry(new Sphere(new Vec3d(-0.8, -2, -6.2), 0.5, red));
 
-// scene1.addGeometry(new Sphere(new Vec3d(-1.5, 1.5, -5), 0.1, white));
-// scene1.addGeometry(new Sphere(new Vec3d(1.5, -1.5, -5), 0.1, white));
-// scene1.addGeometry(new Sphere(new Vec3d(1.5, 1.5, -5), 0.1, white));  
-// scene1.addGeometry(new Sphere(new Vec3d(-1.5, -1.5, -5), 0.1, white));
 
-// scene1.addGeometry(new Sphere(new Vec3d(0, 2, -5.5), 0.5, white));
-// scene1.addGeometry(new Sphere(new Vec3d(0, -2, -5.5), 0.5, white));
-// scene1.addGeometry(new Sphere(new Vec3d(2, 0, -5.5), 0.5, white));
-// scene1.addGeometry(new Sphere(new Vec3d(-2, 0, -5.5), 0.5, white));
+scene1.addLight(new PointLight(new Vec3d(0, 2.49, -7.5), new Color(10, 10, 10), new Vec3d(0, 0, 1)));
+scene1.addLight(new PointLight(new Vec3d(0, 0, -1), new Color(5, 0, 0), new Vec3d(0, 0, 1)));
+scene1.addLight(new PointLight(new Vec3d(1, -2, -9), new Color(0, 0.5, 1), new Vec3d(0, 0, 1)));
 
-scene1.addLight(new PointLight(new Vec3d(0, 4, 0), new Color(0, 30, 30), new Vec3d(0, 0, 1)));
-scene1.addLight(new PointLight(new Vec3d(0, -1, -6), new Color(3, 0, 0), new Vec3d(0, 0, 1)));
-
-scene1.addGeometry(new Triangle(new Vec3d(10, -2.5, -1),
-                                new Vec3d(0, -2.5, -10),  
-                                new Vec3d(-10, -2.5, -1), 
+// floor
+scene1.addGeometry(new Triangle(new Vec3d(2.5, -2.5, 1),
+                                new Vec3d(2.5, -2.5, -10),  
+                                new Vec3d(-2.5, -2.5, 1), 
                                 white));
-// // scene1.addGeometry(new Sphere(new Vec3d(0, -2, -6.3), 0.1, white));
 
-scene1.addGeometry(new Triangle(new Vec3d(0.5, -2.2, -6),
-                                new Vec3d(0, -2.2, -7),
-                                new Vec3d(-0.5, -2.2, -6), 
+scene1.addGeometry(new Triangle(new Vec3d(2.5, -2.5, -10),
+                                new Vec3d(-2.5, -2.5, -10),  
+                                new Vec3d(-2.5, -2.5, 1), 
                                 white));
+
+// ceiling
+scene1.addGeometry(new Triangle(new Vec3d(-2.5, 2.5, 1),
+                                new Vec3d(2.5, 2.5, -10),  
+                                new Vec3d(2.5, 2.5, 1), 
+                                white));
+
+scene1.addGeometry(new Triangle(new Vec3d(-2.5, 2.5, 1),
+                                new Vec3d(-2.5, 2.5, -10),  
+                                new Vec3d(2.5, 2.5, -10), 
+                                white));
+
+// back wall
+scene1.addGeometry(new Triangle(new Vec3d(2.5, 2.5, -10),
+                                new Vec3d(-2.5, 2.5, -10),  
+                                new Vec3d(-2.5, -2.5, -10), 
+                                white));
+
+scene1.addGeometry(new Triangle(new Vec3d(2.5, -2.5, -10),
+                                new Vec3d(2.5, 2.5, -10),  
+                                new Vec3d(-2.5, -2.5, -10), 
+                                white));
+
+// left wall
+scene1.addGeometry(new Triangle(new Vec3d(-2.5, -2.5, -10),
+                                new Vec3d(-2.5, 2.5, 1),  
+                                new Vec3d(-2.5, -2.5, 1), 
+                                white));
+
+scene1.addGeometry(new Triangle(new Vec3d(-2.5, -2.5, -10),
+                                new Vec3d(-2.5, 2.5, -10),  
+                                new Vec3d(-2.5, 2.5, 1), 
+                                white));
+
+// right wall
+scene1.addGeometry(new Triangle(new Vec3d(2.5, -2.5, 1),
+                                new Vec3d(2.5, 2.5, 1),  
+                                new Vec3d(2.5, -2.5, -10), 
+                                white));
+
+scene1.addGeometry(new Triangle(new Vec3d(2.5, 2.5, 1),
+                                new Vec3d(2.5, 2.5, -10),  
+                                new Vec3d(2.5, -2.5, -10), 
+                                white));
+
+// behind camera wall
+scene1.addGeometry(new Triangle(new Vec3d(-2.5, -2.5, 1),
+                                new Vec3d(-2.5, 2.5, 1),  
+                                new Vec3d(2.5, 2.5, 1), 
+                                white));
+
+scene1.addGeometry(new Triangle(new Vec3d(-2.5, -2.5, 1),
+                                new Vec3d(2.5, 2.5, 1),  
+                                new Vec3d(2.5, -2.5, 1), 
+                                white));
+
